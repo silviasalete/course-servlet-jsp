fface2ee543e02340ca0e26d0ccc3f61f5241932
Silvia Salete <silviasalete@users.noreply.github.com>
2020-12-22 11:07:06 -0300

    :rocket: Acrescentado bloco static

--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -7,6 +7,16 @@ public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
 	
+	static {
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Empresa 1");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Empresa 2");
+		
+		lista.add(empresa1);
+		lista.add(empresa2);
+	}
+	
 	public void adiciona(Empresa empresa) { 
 		
 		lista.add(empresa);
