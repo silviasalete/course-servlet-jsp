bfe864af8f2389815a4a8a4959eba0478f676889
Silvia Salete <silviasalete@users.noreply.github.com>
2021-01-13 19:38:55 -0300

    :rocket: Mapping routes whit web.xml

---
+++ src/com/manager/controller/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.manager.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/homeServlet")
+public class HomeServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("<h1>Home</h1>");
+	}
+
+}
