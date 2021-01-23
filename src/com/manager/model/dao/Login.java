78c007140f4b2fd6765fd846cf1d97b6b6e14114
Silvia Salete <silviasalete@users.noreply.github.com>
2021-01-23 12:36:10 -0300

    :rocket: Created Login Class

---
+++ src/com/manager/model/dao/Login.java
@@ -0,0 +1,18 @@
+package com.manager.model.dao;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login implements Action {
+
+	@Override
+	public String performs(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		return null;
+	
+	
+	}
+
+}
