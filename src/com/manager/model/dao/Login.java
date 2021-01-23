e8c0eebdc38c3bf6089b285bf6b8f7884f07f6b6
Silvia Salete <silviasalete@users.noreply.github.com>
2021-01-23 12:42:49 -0300

    :rocket: Created content the of users  class

--- src/com/manager/model/dao/Login.java
+++ src/com/manager/model/dao/Login.java
@@ -10,6 +10,12 @@ public class Login implements Action {
 
 	@Override
 	public String performs(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String    login = request.getParameter("login");
+		String password = request.getParameter("password"); 
+		
+		System.out.println("Logging in "+login);
+		
 		return null;
 	
 	
